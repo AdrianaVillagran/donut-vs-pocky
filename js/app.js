c6a12a9c189de4ee1d1086860af750ceab5a6009
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-19 12:11:34 -0700

    new function to play x

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
+
+  $('div.col-sm-4').on("click", function playTicTacToe(event) {
+      $(this).text("X");
+      event.preventDefault();
+
+  });
 
 });
