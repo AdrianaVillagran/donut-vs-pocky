e7761730c684a077769e7d8762efe233e32e3cbb
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-19 12:53:02 -0700

    added playTicTacToe function that works

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
 
-  $('div.col-sm-4').on("click", function playTicTacToe(event) {
-      $(this).text("X");
-      event.preventDefault();
+
+  var turn = 1;
+  $('div.col-sm-4.box').on('click', function playTicTacToe(event){
+      for(var i=0; i<9; i++) {
+        if ($(this).text() !== 'X' && $(this).text() !== 'O') {
+          if (turn === 1) {
+            $(this).text('X');
+            turn = 2;
+          } else if (turn === 2) {
+            $(this).text('O');
+            turn = 1;
+            }
+        }
+      }
 
   });
+
 
 });
