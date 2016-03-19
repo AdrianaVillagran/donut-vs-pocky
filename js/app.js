f8ce355ef6ce43d071e659069dcf8240b9204997
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-19 14:16:09 -0700

    added reset button and array variable

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
 
+  var ticTacToeBoard = $('.box');
+
 
   var turn = 1;
   $('div.col-sm-4.box').on('click', function playTicTacToe(event){
@@ -8,15 +10,25 @@ $(document).ready(function() {
         if ($(this).text() !== 'X' && $(this).text() !== 'O') {
           if (turn === 1) {
             $(this).text('X');
+            $('#current_player').text("Let's go, O!");
             turn = 2;
           } else if (turn === 2) {
             $(this).text('O');
+            $('#current_player').text("You're turn, X!");
             turn = 1;
+            console.log(ticTacToeBoard.eq(0));
             }
         }
       }
 
   });
+
+  $('button').on('click', function resetBoard(event) {
+    $('.box').text('');
+    turn = 1;
+  });
+
+
 
 
 });
