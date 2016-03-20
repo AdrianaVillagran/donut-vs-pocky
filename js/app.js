85dcb5ea5fbfd40ed20757f795f65395447562d1
AdrianaVillagran <anvillagran1@gmail.com>
2016-03-19 18:44:15 -0700

    got it working

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
+/*
 // wait for the DOM to finish loading
 $(document).ready(function() {
 
-  var ticTacToeBoard = $('.box');
+
 
 
   var turn = 1;
-  $('div.col-sm-4.box').on('click', function playTicTacToe(event){
-      for(var i=0; i<9; i++) {
+  $('.box').on('click', function playTicTacToe(event){
         if ($(this).text() !== 'X' && $(this).text() !== 'O') {
           if (turn === 1) {
             $(this).text('X');
@@ -16,19 +16,87 @@ $(document).ready(function() {
             $(this).text('O');
             $('#current_player').text("You're turn, X!");
             turn = 1;
-            console.log(ticTacToeBoard.eq(0));
             }
         }
-      }
+      isWinner();
+  });
+
+
+
+
+
+*/
+
+
+$(document).ready(function()  {
+
+  var clickCount = 0;
+  var boxCells = $('.box');
+
+  $('.box').on('click', function(event) {
+   if(  ! $(this).text() ){
+     if (clickCount % 2 === 0 ) {
+         $(this).text('X');
+         $('#current_player').text("Let's go, O!");
+         clickCount ++;
+     } else if (clickCount % 2 !== 0) {
+         $(this).text('O');
+         $('#current_player').text("You're turn, X!");
+         clickCount ++;
+       }
+     isWinner();
+   }
 
   });
 
   $('button').on('click', function resetBoard(event) {
-    $('.box').text('');
-    turn = 1;
+    window.location.reload();
+    clickCount = 0;
   });
 
+  function isWinner() {
+      if (($(boxCells.eq(0)).text() === 'X' &&
+           $(boxCells.eq(1)).text() === 'X' &&
+           $(boxCells.eq(2)).text() === 'X') ||
+          ($(boxCells.eq(3)).text() === 'X' &&
+           $(boxCells.eq(4)).text() === 'X' &&
+           $(boxCells.eq(5)).text() === 'X') ||
+          ($(boxCells.eq(6)).text() === 'X' &&
+           $(boxCells.eq(7)).text() === 'X' &&
+           $(boxCells.eq(8)).text() === 'X') ||
+          ($(boxCells.eq(0)).text() === 'X' &&
+           $(boxCells.eq(3)).text() === 'X' &&
+           $(boxCells.eq(6)).text() === 'X') ||
+          ($(boxCells.eq(1)).text() === 'X' &&
+           $(boxCells.eq(4)).text() === 'X' &&
+           $(boxCells.eq(7)).text() === 'X') ||
+          ($(boxCells.eq(2)).text() === 'X' &&
+           $(boxCells.eq(5)).text() === 'X' &&
+           $(boxCells.eq(8)).text() === 'X')) {
+             alert("Player X wins!");
+             $('#current_player').text("");
+           }else if(($(boxCells.eq(0)).text() === 'O' &&
+                    $(boxCells.eq(1)).text() === 'O' &&
+                    $(boxCells.eq(2)).text() === 'O') ||
+                   ($(boxCells.eq(3)).text() === 'O' &&
+                    $(boxCells.eq(4)).text() === 'O' &&
+                    $(boxCells.eq(5)).text() === 'O') ||
+                   ($(boxCells.eq(6)).text() === 'O' &&
+                    $(boxCells.eq(7)).text() === 'O' &&
+                    $(boxCells.eq(8)).text() === 'O') ||
+                   ($(boxCells.eq(0)).text() === 'O' &&
+                    $(boxCells.eq(3)).text() === 'O' &&
+                    $(boxCells.eq(6)).text() === 'O') ||
+                   ($(boxCells.eq(1)).text() === 'O' &&
+                    $(boxCells.eq(4)).text() === 'O' &&
+                    $(boxCells.eq(7)).text() === 'O') ||
+                   ($(boxCells.eq(2)).text() === 'O' &&
+                    $(boxCells.eq(5)).text() === 'O' &&
+                    $(boxCells.eq(8)).text() === 'O')) {
+                      alert("Player O wins!");
+                      $('#current_player').text("");
+                    }
+  }
 
-
-
+  
 });
